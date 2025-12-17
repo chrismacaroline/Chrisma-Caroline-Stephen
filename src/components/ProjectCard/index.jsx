c5d0f5ac9b9c26dc77edda823672d0f7dd2daeb1
Chrisma Stephen <chrismastephen@Chrismas-MacBook-Pro.local>
2025-12-17 23:06:50 +0000

    New sections/card items added under projects to add image and text to explain the project

--- src/components/ProjectCard/index.jsx
+++ src/components/ProjectCard/index.jsx
@@ -7,10 +7,10 @@ import  "../../main.css"
 function ProjectCard(props) {
   return (
     <>
-          <Card xs={12} sm={10} md={8} lg={6} xl={4} className="container-fluid display-flex justify-content-start align-items-start p-4 mb-4">
+          <Card xs={12} sm={10} md={8} lg={6} xl={4} className="container-fluid display-flex p-4 mb-4 border-0 bg-transparent shadow-none" style={{ width: '95%'}}>
             <Card.Body>
-              <Card.Title className="text-left">{props.name}</Card.Title>
-              <Card.Text className="text-left">{props.description}</Card.Text>
+              <Card.Title style={{ fontSize: "1.5rem", paddingBottom: "0.5rem"}} className="text-left">{props.name}</Card.Title>
+              <Card.Subtitle style={{ fontSize: "1.2rem" }}className="text-left">{props.description}</Card.Subtitle>
               <Card.Img variant="top" src={props.image} className="img-fluid" style={{maxWidth: "100%", height: "auto"}} />
               {/* <div className="flex-row flex-sm-row justify-content-around">
                 <a
@@ -36,12 +36,47 @@ function ProjectCard(props) {
                 </a>
               {/* </div> */}
             {/* <Card.Body> */}
-              <Card.Title className="text-left">{props.titleOne}</Card.Title>
-              <Card.Subtitle className="text-left">{props.subtitleOne}</Card.Subtitle>
-              <Card.Text className="text-left">{props.descriptionOne}</Card.Text>
-              <Card.Img src={props.imageOne} className="img-fluid !justify-content-center align-items-center" style={{maxWidth: "60%", height: "60%"}} />
+           <div className="container-fluid" style={{ minHeight: "32rem", padding: "5rem" }}>
+              <div className="row align-items-center">
+                <div className="col-12 col-md-6 text-start p-2">
+                  <Card.Title  style={{ fontSize: "1.5rem" , paddingBottom: "0.5rem"}}>{props.titleOne}</Card.Title>
+                  <Card.Text style={{ fontSize: "1.2rem" , paddingBottom: "0.5rem"}}>{props.subtitleOne}</Card.Text>
+                  <Card.Text style={{ fontSize: "1.2rem" , paddingBottom: "0.5rem"}}>{props.descriptionOne}</Card.Text>
+                </div>
+
+                <div className="col-12 col-md-6 d-flex justify-content-end">
+                  <img
+                    src={props.imageOne}
+                    className="img-fluid"
+                    // style={{ maxWidth: "60%" }}
+                    alt=""
+                  />
+                </div>
+
+            </div>
+          </div>
+
+            <div className="container-fluid" style={{ minHeight: "32rem", padding: "5rem" }}>
+              <div className="row align-items-center">
+                  <div className="col-12 col-md-6 d-flex justify-content-end">
+                  <img
+                    src={props.imageTwo}
+                    className="img-fluid"
+                    // style={{ maxWidth: "60%" }}
+                    alt=""
+                  />
+                </div>
+                <div className="col-12 col-md-6 text-start p-2">
+                  <Card.Title  style={{ fontSize: "1.5rem" , paddingBottom: "0.5rem"}}>{props.titleTwo}</Card.Title>
+                  <Card.Text style={{ fontSize: "1.2rem" , paddingBottom: "0.5rem"}}>{props.subtitleTwo}</Card.Text>
+                  <Card.Text style={{ fontSize: "1.2rem" , paddingBottom: "0.5rem"}}>{props.descriptionTwo}</Card.Text>
+                </div>
+
+            </div>
+          </div>
             </Card.Body>
           </Card>
+              
         
           </>
   );
--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -7,9 +7,9 @@ import "./style.css"
 // eslint-disable-next-line react/prop-types
 function Projects ({id,image,name, description, selectProject}) {
   return (
-    <Card onClick={() => selectProject(id)} className ="container-fluid d-flex flex-column align-items-start justify-content-start py-5"style={{ width: '65%'}}>
-        <Card.Title className="flex-row justify-content-start align-items-start" style={{fontSize: "25px", padding: "10px", color : "#212529", fontFamily : "roboto-medium"}}>{name}</Card.Title>
-        <Card.Subtitle className="flex-row justify-content-start align-items-start" style={{fontSize: "25px", padding: "10px", color : "#212529", fontFamily : "roboto-medium"}}>{description}</Card.Subtitle>
+    <Card onClick={() => selectProject(id)} className ="container-fluid d-flex flex-column align-items-start justify-content-start py-5"style={{ width: '95%'}}>
+        <Card.Title className="flex-row justify-content-start align-items-start" style={{fontSize: "1.5rem", padding: "10px", color : "#212529", fontFamily : "roboto-medium"}}>{name}</Card.Title>
+        <Card.Subtitle className="flex-row justify-content-start align-items-start" style={{fontSize: "1.2rem", padding: "10px", color : "#212529", fontFamily : "roboto-medium"}}>{description}</Card.Subtitle>
         <Card.Img variant="top" src={image} className='img-container flex-column' style = {{width : "100%", height : "75%"}}/>
         <Button id={id}  style ={{backgroundColor: "#f8f9fa" , color : "#212529", margin : "5px", alignItems : "left", fontFamily : "roboto-medium"}} onClick={() => selectProject(id)}>
         FIND OUT MORE
